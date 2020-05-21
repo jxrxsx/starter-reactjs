238c6e477d2205b484a2f173372528c80bfb0518
jxrxsx <jrslord@hotmail.com>
2020-05-21 10:04:01 -0300

    fetching api products

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles.css';
+
+// essa sintaxe é a mesma coisa de
+const Header = () => (
+  <header id="main-header">JSHunt</header>
+);
+
+export default Header;
+
+
+// é o mesmo de 
+/* class Header extends Component {
+  render() {
+    return <h1>salve des</h1>
+  }
+} */
